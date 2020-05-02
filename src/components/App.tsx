e52a92a729d2e8364627bd3284bc4f2b74352bc1
nmc2711 <nmc2711@naver.com>
2020-05-02 16:22:21 +0900

    깃 업로드

---
+++ src/components/App.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const App: React.FC = () => {
+  return <div>황상한 타입스크립트</div>;
+};
+
+export default App;
---
+++ src/components/Greeting.tsx
@@ -0,0 +1,10 @@
+// import React from "react";
+
+// type GreetingsProps = {
+//   name: string;
+// };
+
+// const Greetings: React.FC<GreetingsProps> = ({ name }) => {
+//   <div> HEllo{name}]</div>;
+// };
+// export default Greetings;
